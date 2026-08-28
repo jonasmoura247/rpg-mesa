06923f31150c00ae97e472b58ec32a095cafc73c
jonas <jonas.moura.cavalcante@hotmail.com>
2026-08-28 18:22:52 -0300

    test(creator): cobre caso de floor vs truncamento no modificador negativo

--- docs/creator/js/calculo.test.js
+++ docs/creator/js/calculo.test.js
@@ -5,6 +5,8 @@ assert.strictEqual(Calculo.modificador(10), 0);
 assert.strictEqual(Calculo.modificador(11), 0);
 assert.strictEqual(Calculo.modificador(15), 2);
 assert.strictEqual(Calculo.modificador(8), -1);
+assert.strictEqual(Calculo.modificador(9), -1);
+assert.strictEqual(Calculo.modificador(7), -2);
 assert.strictEqual(Calculo.modificador(20), 5);
 
 console.log('calculo.test.js: OK');
