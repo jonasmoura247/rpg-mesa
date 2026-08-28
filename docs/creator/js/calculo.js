da39faa92436656f4553f3deed7456c806d8cbaf
jonas <jonas.moura.cavalcante@hotmail.com>
2026-08-28 18:24:05 -0300

    feat(creator): calculo de custo e pontos restantes do Point Buy

--- docs/creator/js/calculo.js
+++ docs/creator/js/calculo.js
@@ -3,7 +3,15 @@
     return Math.floor((valorAtributo - 10) / 2);
   }
 
-  const api = { modificador };
+  function custoTotalPointBuy(atributos, tabelaCusto) {
+    return Object.values(atributos).reduce((soma, valor) => soma + tabelaCusto[valor], 0);
+  }
+
+  function pontosRestantes(atributos, tabelaCusto, orcamento) {
+    return orcamento - custoTotalPointBuy(atributos, tabelaCusto);
+  }
+
+  const api = { modificador, custoTotalPointBuy, pontosRestantes };
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = api;
--- docs/creator/js/calculo.test.js
+++ docs/creator/js/calculo.test.js
@@ -9,4 +9,24 @@ assert.strictEqual(Calculo.modificador(9), -1);
 assert.strictEqual(Calculo.modificador(7), -2);
 assert.strictEqual(Calculo.modificador(20), 5);
 
+const tabelaCusto = { 8: 0, 9: 1, 10: 2, 11: 3, 12: 4, 13: 5, 14: 7, 15: 9 };
+
+assert.strictEqual(
+  Calculo.custoTotalPointBuy({ forca: 15, destreza: 14, constituicao: 13, inteligencia: 12, sabedoria: 10, carisma: 8 }, tabelaCusto),
+  27
+);
+assert.strictEqual(
+  Calculo.custoTotalPointBuy({ forca: 8, destreza: 8, constituicao: 8, inteligencia: 8, sabedoria: 8, carisma: 8 }, tabelaCusto),
+  0
+);
+
+assert.strictEqual(
+  Calculo.pontosRestantes({ forca: 8, destreza: 8, constituicao: 8, inteligencia: 8, sabedoria: 8, carisma: 8 }, tabelaCusto, 27),
+  27
+);
+assert.strictEqual(
+  Calculo.pontosRestantes({ forca: 15, destreza: 14, constituicao: 13, inteligencia: 12, sabedoria: 10, carisma: 8 }, tabelaCusto, 27),
+  0
+);
+
 console.log('calculo.test.js: OK');
